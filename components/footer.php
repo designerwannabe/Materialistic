eaafb4d444f3063185813561c71fdaec3e1361e8
designerwannabe <33412037+designerwannabe@users.noreply.github.com>
2017-11-18 11:37:33 +0800

    First phase
    May the odds be ever in our favor. Oh my

---
+++ components/footer.php
@@ -0,0 +1,10 @@
+		<!-- FOOTER -->
+		<script src="assets/scripts/vendor/jquery.js"></script>
+		<script src="assets/scripts/vendor/what-input.js"></script>
+		<script src="assets/scripts/vendor/foundation.js"></script>
+		<script src="assets/scripts/material/floating-label.js"></script>
+		<script src="assets/scripts/material/ripple.js"></script>
+		<script src="assets/scripts/material/switches.js"></script>
+		<script src="assets/scripts/base/canvas.js"></script>
+	</body>
+</html>
---
+++ components/header.php
@@ -0,0 +1,42 @@
+<!doctype html>
+<html class="no-js" lang="en" dir="ltr">
+	<head>
+		<meta charset="utf-8">
+		<meta http-equiv="x-ua-compatible" content="ie=edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title>Materialistic</title>
+		<link rel="stylesheet" href="assets/styles/css/base/app.css">
+		<link rel="stylesheet" href="assets/styles/css/base/nav.css">
+		<link rel="stylesheet" href="assets/styles/css/base/global.css">
+	</head>
+	<body class="theme-light" >
+		<!-- the menu  -->
+		<div class="curtain-menu">
+			<div class="curtain"></div>
+			<div class="curtain"></div>
+			<div class="curtain"></div>
+			<div class="curtain-menu-wrapper">
+				<ul class="curtain-menu-list menu vertical">
+					<li><a href="?p=login">Home</a></li>
+					<li><a href="?p=tables">Profile</a></li>
+					<li><a href="?p=login">Messages</a></li>
+					<li><a href="?p=login">Notification</a></li>
+					<li><a href="?p=login">Logout</a></li>
+				</ul>
+			</div>
+		</div>
+		<div class="md-tabs">
+			<div class="mobile-nav-bar title-bar">
+				<div class="title-bar-left">
+					<div data-curtain-menu-button class="curtain-menu-button">
+						<button class="menu-icon" type="button"></button>
+  					</div>
+				</div>
+				<div class="title-bar-center">
+					<span class="title-bar-text">Designer Wannabe</span>
+				</div>
+				<div class="title-bar-right">
+					<button class="logout-icon" type="button"></button>
+				</div>
+			</div>
+		</div>
